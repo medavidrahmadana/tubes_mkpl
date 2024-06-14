84cc39e1b33d4d2c405555b7a1bf32d16e15f600
YogaElfaraby <100010908+YogaElfaraby@users.noreply.github.com>
2024-06-14 23:09:22 +0700

    Update Main.java

--- src/main/java/praktek_mkpl/Main.java
+++ src/main/java/praktek_mkpl/Main.java
@@ -3,20 +3,66 @@ package praktek_mkpl;
 public class Main {
     public static void main(String[] args) {
         // Membuat objek kalkulator
-        Calculator calculator = new Calculator();
+        Calculator calculatorObjekKalkulator = new Calculator();
+
+        // Variabel untuk operasi kalkulator
+        int angkaPertamaUntukPenjumlahan = 10;
+        int angkaKeduaUntukPenjumlahan = 5;
+        int variabelPenjumlahan = 0; // Duplicated Code & Temporary Field
+        variabelPenjumlahan = calculatorObjekKalkulator.add(angkaPertamaUntukPenjumlahan, angkaKeduaUntukPenjumlahan); // Long Variable Name
         
-        // Melakukan beberapa operasi menggunakan kalkulator
-        int a = 10;
-        int b = 5;
+        System.out.println("Hasil penjumlahan " + angkaPertamaUntukPenjumlahan + " dan " + angkaKeduaUntukPenjumlahan + " adalah: " + variabelPenjumlahan);
+
+        // Pengurangan
+        int angkaPertamaUntukPengurangan = 10; // Duplicated Code
+        int angkaKeduaUntukPengurangan = 5;
+        int variabelPengurangan = calculatorObjekKalkulator.subtract(angkaPertamaUntukPengurangan, angkaKeduaUntukPengurangan);
         
-        System.out.println("Hasil penjumlahan " + a + " dan " + b + " adalah: " + calculator.add(a, b));
-        System.out.println("Hasil pengurangan " + a + " dan " + b + " adalah: " + calculator.subtract(a, b));
-        System.out.println("Hasil perkalian " + a + " dan " + b + " adalah: " + calculator.multiply(a, b));
-        
+        System.out.println("Hasil pengurangan " + angkaPertamaUntukPengurangan + " dan " + angkaKeduaUntukPengurangan + " adalah: " + variabelPengurangan);
+
+        // Perkalian
+        int angkaPertamaUntukPerkalian = 10; // Duplicated Code
+        int angkaKeduaUntukPerkalian = 5;
+        int variabelPerkalian = calculatorObjekKalkulator.multiply(angkaPertamaUntukPerkalian, angkaKeduaUntukPerkalian);
+
+        System.out.println("Hasil perkalian " + angkaPertamaUntukPerkalian + " dan " + angkaKeduaUntukPerkalian + " adalah: " + variabelPerkalian);
+
+        // Pembagian
+        int angkaPertamaUntukPembagian = 10; // Duplicated Code
+        int angkaKeduaUntukPembagian = 5;
+        int variabelPembagian = 0;
+
         try {
-            System.out.println("Hasil pembagian " + a + " dan " + b + " adalah: " + calculator.divide(a, b));
+            variabelPembagian = calculatorObjekKalkulator.divide(angkaPertamaUntukPembagian, angkaKeduaUntukPembagian);
+            System.out.println("Hasil pembagian " + angkaPertamaUntukPembagian + " dan " + angkaKeduaUntukPembagian + " adalah: " + variabelPembagian);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+
+        // Metode untuk cetak hasil
+        cetakHasilPenjumlahan(angkaPertamaUntukPenjumlahan, angkaKeduaUntukPenjumlahan, variabelPenjumlahan);
+        cetakHasilPengurangan(angkaPertamaUntukPengurangan, angkaKeduaUntukPengurangan, variabelPengurangan);
+        cetakHasilPerkalian(angkaPertamaUntukPerkalian, angkaKeduaUntukPerkalian, variabelPerkalian);
+        cetakHasilPembagian(angkaPertamaUntukPembagian, angkaKeduaUntukPembagian, variabelPembagian);
+    }
+
+    // Method untuk cetak hasil penjumlahan
+    private static void cetakHasilPenjumlahan(int a, int b, int hasil) {
+        System.out.println("Penjumlahan " + a + " + " + b + " = " + hasil);
+    }
+
+    // Method untuk cetak hasil pengurangan
+    private static void cetakHasilPengurangan(int a, int b, int hasil) {
+        System.out.println("Pengurangan " + a + " - " + b + " = " + hasil);
+    }
+
+    // Method untuk cetak hasil perkalian
+    private static void cetakHasilPerkalian(int a, int b, int hasil) {
+        System.out.println("Perkalian " + a + " * " + b + " = " + hasil);
+    }
+
+    // Method untuk cetak hasil pembagian
+    private static void cetakHasilPembagian(int a, int b, int hasil) {
+        System.out.println("Pembagian " + a + " / " + b + " = " + hasil);
     }
 }
